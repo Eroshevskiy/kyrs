2f5008c2a763d9d0e95e7ca153965506448329e3
eroshevskiy <eroshevskiy.oleg@mail.ru>
2024-01-20 16:59:13 +0300

    kyrs

---
+++ app/src/main/java/com/example/nastuxa/masters.java
@@ -0,0 +1,13 @@
+package com.example.nastuxa;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class masters extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.masters);
+    }
+}
---
+++ app/src/main/java/com/example/nastuxa/price.java
@@ -0,0 +1,13 @@
+package com.example.nastuxa;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class price extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.price);
+    }
+}
